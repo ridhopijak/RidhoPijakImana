58a6027433811b58419d86fecc4e9a30f9a49492
FlutLab.io <>
2022-04-04 15:59:19 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BelajarAppBar(),
+    );
+  }
+}
+
+class BelajarAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: DefaultTabController(
+        length: 3,
+        child: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                expandedHeight: 200.0,
+                floating: false,
+                pinned: true,
+                flexibleSpace: FlexibleSpaceBar(
+                  centerTitle: true,
+                  title: Text("Belajar SliverAppBar",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      )),
+                  background: Image(
+                    image: AssetImage('assets/image/nirvana.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              SliverPersistentHeader(
+                pinned: true,
+                delegate: _SliverAppBarDelegate(
+                  TabBar(
+                    labelColor: Colors.black87,
+                    unselectedLabelColor: Colors.grey,
+                    tabs: [
+                      new Tab(icon: new Icon(Icons.audiotrack), text: "Sings"),
+                      new Tab(icon: new Icon(Icons.favorite), text: "Songs"),
+                      new Tab(icon: new Icon(Icons.collections), text: "Gallery"),
+                    ],
+                  ),
+                ),
+              ),
+            ];
+          },
+          body: TabBarView(children: <Widget>[
+            BelajarListView(),
+            BelajarListView(),
+            BelajarListView(),
+          ]),
+        ),
+      ),
+    );
+  }
+}
+
+class BelajarListView extends StatelessWidget {
+  final List bulan = [
+    "Smells Like Teen Spirit",
+    "Bred",
+    "All Apologies",
+    "come As You Are",
+    "In Bloom",
+    "Something in the Way",
+    "Lithium",
+    "About A Girl",
+    "Sappy",
+    "Love Buzz",
+    "Endless,Nameles",
+    "Silver"
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+                title: Text(bulan[index], style: TextStyle(fontSize: 30)),
+                subtitle: Text('ini subtitle dari ' + bulan[index]),
+                leading: CircleAvatar(
+                  child: Text(bulan[index][0], // ambil karakter pertama text
+                      style: TextStyle(fontSize: 20)),
+                )),
+          );
+        },
+        itemCount: bulan.length,
+      ),
+    );
+  }
+}
+
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  _SliverAppBarDelegate(this._tabBar);
+
+  final TabBar _tabBar;
+
+  @override
+  double get minExtent => _tabBar.preferredSize.height;
+  @override
+  double get maxExtent => _tabBar.preferredSize.height;
+
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return new Container(
+      child: _tabBar,
+    );
+  }
+
+  @override
+  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
+    return false;
+  }
+}
